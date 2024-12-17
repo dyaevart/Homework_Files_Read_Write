299b9f58f76997fe7cbf26707e97f449db0cc1b0
dyaev <test@test.com>
2024-12-17 22:34:13 +0300

    Добавлен код Задания 3 и перенесены файлы на уровень выше

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pprint
+import os
 
 
 def get_shop_list_by_dishes(dishes, person_count):
@@ -15,19 +16,35 @@ def get_shop_list_by_dishes(dishes, person_count):
 							{"measure": ingredient["measure"], "quantity": int(ingredient["quantity"]) * person_count}
 	return ingredients
 
+def convert_files_content_to_dict(files_paths):
+	file_list = []
+	for file_path in files_paths:
+		with open(file_path, encoding="utf-8") as f:
+			lines = [line for line in f]
+		file_list.append({"file_name": os.path.basename(file_path),
+								"file_lines": len(lines), "file_contain": lines})
+	return sorted(file_list, key=lambda d: d["file_lines"])
 
-with open('../recipes.txt', encoding='utf-8') as file:
+with open("../recipes.txt", encoding="utf-8") as file:
 	cook_book = {}
 	for line in file.read().split('\n\n'):
 		name, _, *args = line.split('\n')
 		cook_li = []
 		for arg in args:
-			arg_list = arg.split(' | ')
+			arg_list = arg.split(" | ")
 			cook_li.append({"ingredient_name": arg_list[0], "quantity": arg_list[1], "measure": arg_list[2]})
 		cook_book[name] = cook_li
 
 	print("Словарь с кулинарной книгой cook_book:\n=============")
 	pprint.pprint(cook_book, sort_dicts=False)
+
 	print("\nПодсчёт количества продуктов:\n=============")
 	pprint.pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
 
+	print("\nОбъединение файлов:\n=============")
+	with open('../result.txt', 'w', encoding='utf-8') as f:  # w в явном виде указывает, что мы хотим открыть файл для записи
+		all_files_list = convert_files_content_to_dict(["../1.txt", "../2.txt", "../3.txt"])
+		for file_item in all_files_list:
+			f.write(file_item["file_name"] + "\n"
+					+ str(file_item["file_lines"]) + "\n"
+					+ "".join(file_item["file_contain"]) + "\n")
